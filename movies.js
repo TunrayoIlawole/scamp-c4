e3d59c88732aa0a271d2ae6c51fb25a28fd74c83
Motunrayo Ilawole <tunrayoilawole99@gmail.com>
2021-04-30 16:36:04 +0100

    add filter functionality

--- movies.js
+++ movies.js
@@ -1,5 +1,6 @@
 const moviesContainer = document.querySelector('.movies-container');
 const loaderCon = document.querySelector('.loader');
+const movieSearch = document.querySelector('input'); 
 
 let requestFile = 'https://api.themoviedb.org/3/movie/popular?api_key=f3f08c027c288ebf1a09b168451ce353&language=en-US&page=1';
 
@@ -11,13 +12,33 @@ const loader = `
 </div>
 `;
 
-const cutText = (text) => {
-    if (text.length > 200) {
-        const main = text.slice(0, 200);
+const cutText = (text, length) => {
+    if (text.length > length) {
+        const main = text.slice(0, length);
         return `${main}...`;
     } else {
         return text;
     }
+}
+
+const displayResult = (data) => {
+    data.forEach(movie => {
+        let imagePath = "https://image.tmdb.org/t/p/w500/";
+        const movieTitle = cutText(movie.original_title, 12);
+        const movieText = cutText(movie.overview, 150);
+
+        const movieCon = `
+            <div class = "film" id = ${movie.id}>
+                <img src = ${imagePath}${movie.poster_path} >
+                <div class = "movie-details">
+                    <h2>${movieTitle}</h2>
+                    <p>${movieText}</p>
+                    <a class = "read-more" href = "#">Read more...</a>
+                </div>
+            </div>
+        `;
+        moviesContainer.insertAdjacentHTML('beforeend', movieCon);
+    })
 }
 
 
@@ -37,24 +58,30 @@ const fetchMovies = async () => {
             loader.parentElement.removeChild(loader);
         }
 
-        data.results.forEach(movie => {
-            let imagePath = "https://image.tmdb.org/t/p/w500/";
-            const movieText = cutText(movie.overview);
+        displayResult(data.results);
+    } catch (error) {
+        console.log(error);
+    }
+}
 
-            const movieCon = `
-            <div class = "film">
-                <img src = ${imagePath}${movie.poster_path} >
-                <div class = "movie-details">
-                    <h2>${movie.original_title}</h2>
-                    <p>${movieText}</p>
-                </div>
-            </div>
-            `;
-            moviesContainer.insertAdjacentHTML('beforeend', movieCon);
-        })
+const searchByName = async () => {
+    moviesContainer.innerHTML = "";
+    let value = movieSearch.value.toLowerCase();
+
+    try {
+        const result = await fetch(requestFile);
+        const data = await result.json();
+
+        const returnedData = data.results.filter((word) => {
+            return word.original_title.toLowerCase().includes(value);
+        });
+        console.log(returnedData);
+
+        displayResult(returnedData);
     } catch (error) {
         console.log(error);
     }
 }
 
 window.addEventListener('load', fetchMovies);
+movieSearch.addEventListener('input', searchByName);
